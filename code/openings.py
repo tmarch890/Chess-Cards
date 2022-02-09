06dd5815c06e321385c678fb6e7c9b4c44bba469
tmarch890 <94567562+tmarch890@users.noreply.github.com>
2022-02-09 00:41:10 -0500

    Add files via upload

--- code/openings.py
+++ code/openings.py
@@ -1,1 +1,21 @@
-opening_dictionary = {'1. e4': {'e4-e5': {'Vienna Gambit': [['1.Pe4 2.pe5 3.Nc3 4.nf6 5.Pf4']], 'Ruy López': [['1.Pe4 2.pe5 3.Nf3 4.nc6 5.Bb5']]}, 'Sicillian': {'Open Sicillian': [['1.Pe4 2.pc5 3.Nf3 4.nc6 5.Pd4 6.pxd4 7.Nxd4 ', ['8.nf6 9.Nc3 ', ['10.pd6 ', ['11.Bg5 12.pe6 13.Qd2 14.pa6', '11.Bc4', '11.Be2'], '10.pe5 11.Nb5d4 12.pd6 13.Bg5 14.pa6 15.Na3', '10.pe6 '], '8.pe6 9.Nc3 ', ['10.qc7', '10.nf6', '10.pa6']]]], 'Closed Sicillian': [['1.Pe4 2.pc5 3.Nc3 4.nc6 5.Nf3']]}}, '1. d4': {"Queen's Gambit": {'Slav Defense': [['1.Pd4 2.pd5 3.Pc4 4.pc6 5.Nf3']], 'Declined': [['1.Pd4 2.pd5 3.Pc4 4.pe6 5.Nc3']]}, 'London': [['1.Pd4 2.pd5 3.Bf4 4.nc6 5.Nf3 6.bf5']], 'Trompowsky': [['1.Pd4 2.nf6 3.Bg5 4.ne4 5.Bf4']]}}
+opening_dictionary = {
+    '1. e4': {
+        'e4-e5': {
+            'Vienna Gambit': [['1.Pe4 2.pe5 3.Nc3 4.nf6 5.Pf4']],
+            'Ruy López': [['1.Pe4 2.pe5 3.Nf3 4.nc6 5.Bb5']]
+            },
+        'Sicillian': {
+        'Open Sicillian': [[
+            '1.Pe4 2.pc5 3.Nf3 4.nc6 5.Pd4 6.pxd4 7.Nxd4 ', ['8.nf6 9.Nc3 ', ['10.pd6 ', ['11.Bg5 12.pe6 13.Qd2 14.pa6', '11.Bc4', '11.Be2'], '10.pe5 11.Nb5d4 12.pd6 13.Bg5 14.pa6 15.Na3', '10.pe6 '], '8.pe6 9.Nc3 ', ['10.qc7', '10.nf6', '10.pa6']]
+            ]],
+        'Closed Sicillian': [['1.Pe4 2.pc5 3.Nc3 4.nc6 5.Nf3']]
+        }
+    },
+    '1. d4': {
+        "Queen's Gambit": {
+            'Slav Defense': [['1.Pd4 2.pd5 3.Pc4 4.pc6 5.Nf3']], 'Declined': [['1.Pd4 2.pd5 3.Pc4 4.pe6 5.Nc3']]
+        },
+        'London': [['1.Pd4 2.pd5 3.Bf4 4.nc6 5.Nf3 6.bf5']],
+        'Trompowsky': [['1.Pd4 2.nf6 3.Bg5 4.ne4 5.Bf4']]
+    }
+}
